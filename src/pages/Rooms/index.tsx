5a43f2dc46e8d297cfd41d998f105f2e1bc5f964
Julia Adikhanyan <jules.adikhanyan@gmail.com>
2022-05-15 19:03:31 +0300

    Create title rooms page

--- src/pages/Rooms/index.tsx
+++ src/pages/Rooms/index.tsx
@@ -1,8 +1,33 @@
 import React from "react";
+import { Typography, Box, styled } from "@mui/material";
+
+const StyledTypography = styled(Typography)({
+    textAlign: "center"
+});
 
 const Rooms: React.FC = () => {
     return (
-        <h1>QROOM</h1>
+        <Box>
+            <StyledTypography
+                sx={{
+                    fontSize: 72,
+                    fontWeight: "bold"
+                }}
+            >
+                QROOM
+            </StyledTypography>
+            <StyledTypography>
+                Scan the QR code of the meeting room and choose the time for booking
+            </StyledTypography>
+            <StyledTypography
+                sx={{
+                    marginTop: 5,
+                    fontWeight: "bold"
+                }}
+            >
+                LIST OF ROOMS
+            </StyledTypography>
+        </Box>
     )
 }
 
