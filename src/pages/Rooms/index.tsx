91c80bb8a9db88a162a7a478a7df3b2a5b97bbfc
Julia Adikhanyan <jules.adikhanyan@gmail.com>
2022-05-15 19:24:27 +0300

    Create list of rooms

--- src/pages/Rooms/index.tsx
+++ src/pages/Rooms/index.tsx
@@ -1,13 +1,30 @@
 import React from "react";
-import { Typography, Box, styled } from "@mui/material";
+import { Typography, Box, styled, Grid } from "@mui/material";
+
+import theme from "../../style/theme";
 
 const StyledTypography = styled(Typography)({
     textAlign: "center"
 });
 
+const listOfRooms: Array<string> = [
+    "Seattle",
+    "Chicago",
+    "Atlanta",
+    "Boston",
+    "Dallas",
+    "Los Angeles",
+    "Houston",
+    "Kansas City",
+    "Las Vegas",
+    "New Orleans",
+    "New York",
+    "Phoenix"
+];
+
 const Rooms: React.FC = () => {
     return (
-        <Box>
+        <Box sx={{ marginBottom: "20px" }}>
             <StyledTypography
                 sx={{
                     fontSize: 72,
@@ -21,12 +38,35 @@ const Rooms: React.FC = () => {
             </StyledTypography>
             <StyledTypography
                 sx={{
-                    marginTop: 5,
+                    margin: "20px 0 20px 0",
                     fontWeight: "bold"
                 }}
             >
                 LIST OF ROOMS
             </StyledTypography>
+            <Grid
+                container
+                spacing={2}
+                columns={12}
+                sx={{
+                    margin: "auto"
+                }}
+            >
+                {
+                    listOfRooms.map((room) =>
+                        <Grid key={room} item xs={4}>
+                            <Box sx={{
+                                width: 290,
+                                height: 350,
+                                backgroundColor: theme.palette.primary.main,
+                                borderRadius: 2,
+                                cursor: "pointer"
+                            }}>
+                            </Box>
+                        </Grid>
+                    )
+                }
+            </Grid>
         </Box>
     )
 }
