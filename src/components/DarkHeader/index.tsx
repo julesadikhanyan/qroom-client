63d8d076e65645027dcacc03aaeebb2088204ba0
Julia Adikhanyan <jules.adikhanyan@gmail.com>
2022-05-15 19:38:33 +0300

    Style: dark header

--- src/components/DarkHeader/index.tsx
+++ src/components/DarkHeader/index.tsx
@@ -1,28 +1,50 @@
 import React from "react";
-import { AppBar, Box, Button, Toolbar, Typography, Stack } from "@mui/material";
+import { AppBar, Box, Button, Toolbar, Typography, Stack, styled } from "@mui/material";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+import theme from "../../style/theme";
+
+const StyledButton = styled(Button)({
+    border: "1px solid #FFFFFF"
+});
 
 const DarkHeader: React.FC = () => {
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
+            <AppBar position="static" sx={{ padding: "0 20px 0 20px" }}>
                 <Toolbar>
                     <Stack
-                        sx={{ flexGrow: 1}}
-                        spacing={2}
+                        sx={{ flexGrow: 1, alignItems: "center"}}
+                        spacing={3}
                         direction="row"
                     >
-                        <Button color="inherit">
+                        <Button sx={{
+                            backgroundColor: "#FFFFFF",
+                            color: theme.palette.primary.main,
+                            height: 30,
+                            "&:hover": {
+                                backgroundColor: "#FFFFFF"
+                            }
+                        }}>
                             <ArrowBackIcon/>
                         </Button>
-                        <Typography component="div">QR</Typography>
+                        <Typography
+                            component="div"
+                            sx={{
+                                fontSize: 36,
+                                fontWeight: "bold",
+                                color: "#FFFFFF"
+                            }}
+                        >
+                            QR
+                        </Typography>
                     </Stack>
                     <Stack
                         spacing={2}
                         direction="row"
                     >
-                        <Button color="inherit">Sign up</Button>
-                        <Button color="inherit">Log in</Button>
+                        <StyledButton color="inherit">Sign up</StyledButton>
+                        <StyledButton color="inherit">Log in</StyledButton>
                     </Stack>
                 </Toolbar>
             </AppBar>
