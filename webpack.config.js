61ad3c8d2653e0ff86010c8ccde6a2f7e1363b98
Julia Adikhanyan <jules.adikhanyan@gmail.com>
2022-05-18 12:19:58 +0300

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     mode: "production",
